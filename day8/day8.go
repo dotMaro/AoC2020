ccc7ef0e16cf3268f846f405bbc01e41fcc138f0
MattiasBerlin <mattias-berlin@hotmail.com>
2020-12-08 12:45:55 +0100

    Add day 8 solution

---
+++ day8/day8.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/dotMaro/AoC2020/utils"
+)
+
+func main() {
+	input := utils.ReadFile("day8/input.txt")
+
+	task1, task2 := make(chan struct{}), make(chan struct{})
+	go func(chan struct{}) {
+		_, acc := run(input)
+		utils.Print("Task 1. Acc on duplicate instruction is %d", acc)
+		close(task1)
+	}(task1)
+
+	go func(chan struct{}) {
+		acc := repair(input)
+		utils.Print("Task 2. Acc on reparation is %d", acc)
+		close(task2)
+	}(task2)
+	<-task1
+	<-task2
+}
+
+type interpreter struct {
+	pc  int
+	acc int
+}
+
+// run a program and return if it was a successful execution and the acc value.
+// Even if the execution is not successful acc will be returned.
+func run(s string) (bool, int) {
+	i := interpreter{}
+	instructionsRun := make(map[int]struct{})
+	splitLines := utils.SplitLine(s)
+	for {
+		if i.pc == len(splitLines) {
+			// Successful execution
+			return true, i.acc
+		}
+		if i.pc < 0 || i.pc > len(splitLines) {
+			// Illegal jmp, failure
+			return false, i.acc
+		}
+		line := splitLines[i.pc]
+		_, hasRun := instructionsRun[i.pc]
+		if hasRun {
+			// Loop, failure
+			return false, i.acc
+		}
+		// Nothing uses the set during execution so it is safe to set before the actual execution
+		instructionsRun[i.pc] = struct{}{}
+		switch line[:3] {
+		case "acc":
+			value, _ := strconv.Atoi(line[5:])
+			switch line[4] {
+			case '+':
+				i.acc += value
+			case '-':
+				i.acc -= value
+			}
+			i.pc++
+		case "jmp":
+			value, _ := strconv.Atoi(line[5:])
+			if value == 0 {
+				// 0 jump, failure
+				return false, i.acc
+			}
+			switch line[4] {
+			case '+':
+				i.pc += value
+			case '-':
+				i.pc -= value
+			}
+		case "nop":
+			i.pc++
+		}
+	}
+}
+
+func repair(s string) int {
+	for ix := 0; ix < strings.Count(s, "\n"); ix++ {
+		// First try replacing the nth jmp with nop
+		normal, acc := run(replaceNth(s, "jmp", "nop", ix))
+		if normal {
+			return acc
+		}
+		// Then the nth nop with jmp
+		normal, acc = run(replaceNth(s, "nop", "jmp", ix))
+		if normal {
+			return acc
+		}
+	}
+	return -1
+}
+
+func replaceNth(s string, old, new string, n int) string {
+	occurrence := 0
+	i := 0
+	for i < len(s) {
+		offset := strings.Index(s[i:], old)
+		i += offset
+		if occurrence == n {
+			return s[:i] + strings.Replace(s[i:], old, new, 1)
+		}
+		i += len(old)
+		occurrence++
+	}
+	return s
+}
---
+++ day8/day8_test.go
@@ -0,0 +1,58 @@
+package main
+
+import "testing"
+
+const input = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
+func TestInterpreterRun(t *testing.T) {
+	_, res := run(input)
+	if res != 5 {
+		t.Errorf("Should return 5, but returned %d", res)
+	}
+}
+
+func TestInterpreterRepair(t *testing.T) {
+	res := repair(input)
+	if res != 8 {
+		t.Errorf("Should return 8, but returned %d", res)
+	}
+}
+
+func TestReplaceNth(t *testing.T) {
+	const inputReplaced = `nop +0
+acc +1
+nop +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+
+	testCases := []struct {
+		s        string
+		old, new string
+		n        int
+		expect   string
+	}{
+		{"aaaa", "a", "b", 0, "baaa"},
+		{"aaaa", "a", "b", 1, "abaa"},
+		{"aaaa", "a", "b", 2, "aaba"},
+		{input, "jmp", "nop", 0, inputReplaced},
+	}
+
+	for _, tc := range testCases {
+		res := replaceNth(tc.s, tc.old, tc.new, tc.n)
+		if res != tc.expect {
+			t.Errorf("Should return %s but returned %s", tc.expect, res)
+		}
+	}
+}
